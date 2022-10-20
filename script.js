3938c276610ec851949a9b6ad89bea065a80e73c
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-10-20 14:22:43 +0300

    Add functionality to the delete key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+
 function clickNumber(e) {
     console.log(display1.textContent);
     display1.textContent = display1.textContent 
@@ -11,8 +11,16 @@ function clickPoint(e) {
         + e.target.textContent;
     }
 }
+function clickDelete() {
+    display1.textContent = display1.textContent.slice(0, -1);
+}
+
 
 let floatingPoint = false;
+const buttons = document.querySelectorAll('button');
+const display1 = document.querySelector('.display1');
+
+
 buttons.forEach((element) => {
     if(Number.isInteger(+element.textContent) == true ) {
         element.addEventListener('click', clickNumber);
@@ -20,6 +28,8 @@ buttons.forEach((element) => {
     if(element.textContent === '.') {
         element.addEventListener('click', clickPoint);
     }
-});
+    if(element.textContent === 'Delete') {
+        element.addEventListener('click', clickDelete);
+    }
 
-const display1 = document.querySelector('.display1');
+});
