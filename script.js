445ddba019277935f3903a4f578afcf7ed595f26
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-10-20 16:25:54 +0300

    Improve operator keys functionality

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 function clickNumber(e) {
     console.log(display1.textContent);
-    display1.textContent = display1.textContent 
-    + e.target.textContent;
+    if(display1.textContent !== '0' && display1.textContent.length < 19 
+    && display1.textContent !== 'Infinity') {
+        display1.textContent = display1.textContent 
+        + e.target.textContent;
+    }
+    else if (display1.textContent.length < 19 
+        && display1.textContent !== 'Infinity'){
+        display1.textContent = e.target.textContent;
+    }
+    else {
+        display1.textContent = "Infinity";
+    }
 }
 function clickPoint(e) {
     if(floatingPoint === false) {
@@ -18,27 +28,28 @@ function clickOperator(e) {
     floatingPoint = false;
     switch(prevClick) {
         case '':
-            display2.textContent = display1.textContent + " " + e.target.textContent;
+            display2.textContent = display1.textContent + " " 
+            + e.target.textContent;
             break;
         case '+':
             display2.textContent = +(display2.textContent.slice(0, -2))
-            + +display1.textContent;
+            + +display1.textContent + " " + e.target.textContent;
             break;
         case '-':
             display2.textContent = +(display2.textContent.slice(0, -2))
-            - +display1.textContent;
+            - +display1.textContent + " " + e.target.textContent;;
             break;
         case '*':
             display2.textContent = +(display2.textContent.slice(0, -2))
-            * +display1.textContent;
+            * +display1.textContent + " " + e.target.textContent;;
             break;
         case '%':
             display2.textContent = +(display2.textContent.slice(0, -2))
-            / +display1.textContent;
+            / +display1.textContent + " " + e.target.textContent;;
             break;
     }
     prevClick = e.target.textContent;
-    display1.textContent = '';
+    display1.textContent = '0';
 }
 function clickEqual() {
     floatingPoint = false;
